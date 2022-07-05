b2b28cdf436fff9466de799578f4a80271f222f3
MirlanTerenov <kakamern@mail.ru>
2022-07-05 17:28:42 +0600

    Create models,admin, index,forms, urls

--- git_practice/webapp/admin.py
+++ git_practice/webapp/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from .models import Article
+from .models import Product
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['id', 'phone', 'description', 'category', 'count', 'price']
-    list_filter = ['phone']
+    list_filter = ['category']
     search_fields = ['phone', 'category']
     fields = ['phone', 'description', 'category', 'count', 'price']
     list_display_links = ['id', 'phone', 'category']
 
 
-admin.site.register(Article, ProductAdmin)
+admin.site.register(Product, ProductAdmin)
